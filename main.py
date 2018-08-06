d628b03856f94ab701414f70c013da7460b2f161
hag007 <hagai.levi.007@gmail.com>
2018-08-06 14:34:49 +0300

    go skeleton

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ for cur_tested_file in ["protein_coding_long.txt"]:
             # vae_mesh_obj = VAEmesh(gene_expression_top_var_rotated.shape[1])
             # vae_mesh_obj.build_mesh()
             # vae_mesh_obj.train_mesh(gene_expression_top_var_rotated, labels_assignment[0])
-            roots = ['GO:0005575']
+            roots = ['GO:0044429'] # ['GO:0005575']
             dict_result, go2geneids, geneids2go, get_entrez2ensembl_dict = build_hierarcy(roots)
             vae_go_obj = VAEgo(gene_expression_top_var_rotated.shape[1])
-            vae_go_obj.build_go(gene_expression_top_var_headers_rows, go2geneids, geneids2go, dict_result[0]['GO:0005575']['vertices'], dict_result[0]['GO:0005575']['edges'])
+            vae_go_obj.build_go(gene_expression_top_var_headers_rows, go2geneids, geneids2go, dict_result[0][roots[0]]['vertices'], dict_result[0][roots[0]]['edges'])
             # vae_obj.train_mesh(gene_expression_top_var_rotated, labels_assignment[0])
 
 
