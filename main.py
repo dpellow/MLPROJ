b38d3e337c83954e32bd1edaf8a9d859d2f7cb0a
David Pellow <dpellow@localhost.localdomain>
2018-08-30 11:55:32 +0300

    Fixing p-value for permutations

--- main.py
+++ main.py
@@ -91,14 +91,14 @@ for cur_tested_file in ["protein_coding_long.txt"]:
                 gene_expression_top_var_permuted_rotated = np.rot90(np.flip(gene_expression_top_var_permuted, 1), k=-1, axes=(1, 0))
 		        ### NEED TO ADD PARAM TO CHOOSE WHERE TO WRITE OUTPUT FILE
                 vae_go_obj.train_go(gene_expression_top_var_headers_rows, gene_expression_top_var_permuted_rotated, survival_dataset[:, 1]) ### DO WE NEED TO RUN build_go AGAIN TOO??
-		        ### NEED TO OUTPUT SINGLE NUMBER FROM SURVIVAL (P-VALUE?)
-                pvals.append(find_clusters_and_survival(reduced_dim_file_name=reduced_dim_file_name,
+                lr = (find_clusters_and_survival(reduced_dim_file_name=reduced_dim_file_name,
                                             total_gene_list_file_name=None,
                                             gene_expression_file_name=gene_expression_file_name,
                                             phenotype_file_name=phenotype_file_name, survival_file_name=survival_file_name,
                                             var_th_index=var_th_index, is_unsupervised=True, start_k=app_config["start_k"],
                                             end_k=app_config["end_k"], filter_expression=filter_expression, meta_groups=meta_groups,
                                             clustering_algorithm=app_config["clustering_algorithm"]))
+                pvals.append(lr[0])
 	        avg = sum(pvals)/float(len(pvals))
 
 
