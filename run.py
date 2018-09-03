2cad5434f6616c59eb7053cabec9e1ae9f4074fb
David Pellow <dpellow@localhost.localdomain>
2018-09-04 01:05:19 +0300

    Runs

--- run.py
+++ run.py
@@ -8,36 +8,40 @@ import time
 from utils.aggregator import aggregate_all
 from utils.aggregator import clear_all
 
-latent_dims = [5]# ,5,10,20,30,50,75,100]
-epochs = [100]
+epochs = [1,20,50,100,200]
+
+#David's run 1
+thresholds = [100,500]
+num_neurons = [5,10]
+latent_dims = [2,5]
+
+#David's run 2
+#thresholds = [100,500]
+#num_neurons = [50]
+#latent_dims = [2,5,10,50]
+
+#David's run 3 
+#thresholds = [100,500]
+#num_neurons = [100]
+#latent_dims = [2,5,10,50]
 
 
+#Asia run 1
+thresholds=[1500]
+num_neurons=[5,10]
+latent_dims=[2,5]
+
+#asia run 2
+#thresholds=[1500]
+#num_neurons = [50]
+#latent_dims = [2,5,10,50]
+
+#asia run 3
+#thresholds=[1500]
+#num_neurons = [100]
+#latent_dims = [2,5,10,50]
 
 
-
-
-#thresholds = [2000,5000,10000,15000,20000]
-#num_neurons = [100,75,50,30,20,10,5]
-
-
-#David's run
-#thresholds = [2000,5000,10000]#,15000,20000]
-#num_neurons = [100,75,50]#,30,20,10,5]
-
-
-#Hagai's run
-
-thresholds = [100,] # , 2000,5000,10000]#,15000,20000]
-num_neurons = [30]# ,20,10,5]
-
-
-
-
-
-
-#Asia's run
-#thresholds = [15000,20000]
-#num_neurons = [100,75,50,30,20,10,5]
 
 clear_all(latent_dims, thresholds, num_neurons)
 
@@ -46,6 +50,7 @@ rows_output=[]
 for d in latent_dims:
     for t in thresholds:
         for n in num_neurons:
+	  if n < d: continue
             ##result proc
             results = main.run(var_th_index=t,number_of_neurons=n, latent_dim=d, num_of_epochs=epochs)
 
